6877897cc3e1ba3966ef541373553a92ec1c9acd
theoyu314159 <125031865+theoyu314159@users.noreply.github.com>
2024-11-17 05:29:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    html=''
+    with open('index.html',"r",encoding="utf-8") as f:
+        html = f.read()
+    return html
+@app.route('/url', methods=['POST'])
+def submit():
+    html=''
+    with open('index.html',"r",encoding="utf-8") as f:
+        html = f.read()
+    submit = request.form.get('url')
+    path=f'{submit}.txt'
+    with open( path,'w',encoding="utf-8") as f:
+        pass
+    with open( path,'a',encoding="utf-8") as f:
+        f.write('welcome'+'\n')
+    return  html.replace('<out/>',f'http://192.168.1.109:5000/{path}')
+
+@app.route('/<string:filen>', methods=['GET','POST'])
+def do(filen):
+    html=''
+    chatout=''
+    with open('chat.html',"r",encoding="utf-8") as f:
+        html = f.read()
+    if request.method == 'POST':
+        now=str(datetime.datetime.utcnow())
+        username=request.form.get('username')
+        input=request.form.get('input')
+        #print(input)
+        with open(filen, "a", encoding="utf-8") as f:
+                    f.write(now+username+'--'+input+'\n')
+    with open(filen,"r",encoding="utf-8") as f:
+        chatout=f.read()
+    chatout = chatout.replace("\n", "<br>")
+    return  html.replace('<out/>',chatout)
+
+app.run(debug=True, host='0.0.0.0')
